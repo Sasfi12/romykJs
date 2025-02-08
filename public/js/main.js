d0833f368faf7d1e04a0885220e47428f2993a35
Sasfi12 <sufyan10.muhammad1070@hotmail.com>
2025-02-09 00:31:16 +0100

    modal script finished

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,49 @@
-// darkMode 
+// DEBUT DARKMODE 
 let darkMode = document.querySelector("#darkMode");
 let allInsideBody = document.body.querySelectorAll("*");
 let allBody = document.querySelector("body");
 function darkmodetoggle() {
     allBody.classList.toggle("bg-dark");
     allBody.classList.toggle("text-white");
+    document.querySelector(".logos").children[0].classList.toggle("text-white")
+    document.querySelector(".logos").children[1].classList.toggle("text-white")
     allInsideBody.forEach(function(e) {
         if(!e.classList("bg-primary")) {
-        e.classList.toggle("bg-dark");
+            e.classList.toggle("bg-dark");
         }
     })
 }
 darkMode.addEventListener("click", darkmodetoggle);
-
-// modal 
+// FIN DARKMODE
+// DEBUT MODAL
 let loginButton = document.querySelector("#loginButton");
-loginButton.addEventListener("click" ,function() {alert("open modal")})
-//carousel 
-
+let loginModal = document.querySelector("#Login")
+loginButton.addEventListener("click" ,function() {
+    loginModal.classList.add("show-modal")
+})
+let registerOption = document.querySelectorAll(".registerOption")
+let registerModal = document.querySelector("#Register")
+registerOption.forEach(function(e) {
+    e.addEventListener("click", function() {
+        if(loginModal.classList.contains("show-modal")) {
+            loginModal.classList.remove("show-modal");
+            registerModal.classList.add("show-modal"); 
+        }
+        else if(registerModal.classList.contains("show-modal")) {
+            loginModal.classList.add("show-modal");
+            registerModal.classList.remove("show-modal");  
+        }
+    })
+})
+let modalCloseButton = document.querySelectorAll(".modalCloseButton") 
+modalCloseButton.forEach(function(e) {
+    e.addEventListener("click", function() {
+        console.log(e); 
+        console.log(e.parentNode.parentNode.id)
+        e.parentNode.parentNode.classList.remove("show-modal")
+    })
+})
+// FIN MODAL
 // burgerMenu 
 let burgerMenu = document.querySelector("#bourgir");
 let burgeredPart = document.querySelector("#burgeredPart");
@@ -25,7 +51,8 @@ burgerMenu.addEventListener("click", function() {
     burgeredPart.classList.toggle("burger-content")
 })
 setInterval(function() {
-    if(window.innerWidth < 850) {
-        burgerMenu.classList.remove("burger-content")
+    if(window.innerWidth > 850) {
+        burgeredPart.classList.remove("burger-content")
     }
 }, 200  )
+//carousel 
